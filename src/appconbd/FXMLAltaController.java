3c146f9d9979200d465eb2b0d4e63e960a34efe1
Miguel <Miguel@LAPTOP-UFI19A04>
2020-10-30 14:23:37 +0100

    Alta y consulta

--- src/appconbd/FXMLAltaController.java
+++ src/appconbd/FXMLAltaController.java
@@ -5,9 +5,25 @@
  */
 package appconbd;
 
+import basedatos.ConexionMySQL;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,13 +32,101 @@ import javafx.fxml.Initializable;
  */
 public class FXMLAltaController implements Initializable {
 
+    Connection conn = null;
+    ResultSet rs = null;
+    PreparedStatement pst = null;
+    
+    @FXML
+    private TextField txtFieldNombreAlta;
+
+    @FXML
+    private TextField txtFieldAppellidosAlta;
+        
+    @FXML
+    private TextField txtFieldEmailAlta;
+            
+    @FXML
+    private TextField txtFieldDniAlta;
+  
+    @FXML
+    private ScrollPane scrollPaneConsultaAlta;
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        System.out.println("modificación");
-    }    
+        conn = ConexionMySQL.ConnecrDb();               // Obtener la conexión de la base de datos
+    }  
+    
+    /*
+    * Método para dar de alta un empleado en base de datos.
+    * Se ejecuta cuando se pulse el botón de dar de alta.
+    @param event evento
+    */
+    @FXML
+    public void altaEmpleado(ActionEvent event) {
+        System.out.println("FXMLAltaController - altaEmpleado - inicio");
+        
+        //CREAR SENTENCIA SQL DE INSERCIÓN        
+        String sql = "INSERT INTO empleados (dni,nombre,apellidos,email) VALUES('"
+                +txtFieldDniAlta.getText()+"','"+txtFieldNombreAlta.getText()+"','"
+                +txtFieldAppellidosAlta.getText()+"','"+txtFieldEmailAlta.getText()+"');";
+        
+        int resultadoAlta = 0;
+        try {
+            pst = conn.prepareStatement(sql);
+            //EJECUTAR SENTENCIA DE INSERCIÓN SOBRE LA BASE DE DATOS
+            resultadoAlta = pst.executeUpdate(sql); //En rs estará el valor de la tabla
+            conn.close();                    //Cerrar la conexión
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLAltaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("resultado="+resultadoAlta);                                  
+        System.out.println("FXMLAltaController - altaEmpleado - fin");
+    }
+    
+    
+        /*
+    * Método para mostrar los empleados de base de datos en pantalla.
+    @param event evento
+    */
+    @FXML
+    public void consultaEmpleados(ActionEvent event) {
+        System.out.println("FXMLAltaController - consultaEmpleados - inicio");
+        //CREAR SENTENCIA SQL DE INSERCIÓN        
+        String sql = "SELECT * FROM empleados";
+        try {
+            pst = conn.prepareStatement(sql);
+            rs = pst.executeQuery(sql); //En rs estará el valor de la tabla
+            
+            VBox filas = new VBox();            // Caja vertical donde iremos metiendo las cajas horizontales
+            HBox tabla;
+
+            //AGREGAR LOS EMPLEADOS DE BASE DE DATOS AL PANEL SCROLL
+            while (rs.next()) {                            //Mientras haya filas de la tabla
+                System.out.println(rs);
+                //Text dniText = new Text(rs.getString("dni"));
+                tabla = new HBox();
+                tabla.getChildren().addAll(new Text(rs.getString("dni")), new Text(rs.getString("nombre")),
+                        new Text(rs.getString("apellidos")), new Text(rs.getString("email")));
+                tabla.setSpacing(10);
+                tabla.setPadding(new Insets(10));
+                
+                filas.getChildren().addAll(tabla);
+                
+            }
+            scrollPaneConsultaAlta.setContent(filas);
+            
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLAltaController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        
+        
+        System.out.println("FXMLAltaController - consultaEmpleados - fin");
+    }
     
 }
--- src/appconbd/FXMLDocumentController.java
+++ src/appconbd/FXMLDocumentController.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,74 +16,87 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 /**
  * @author Miguel
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     Connection conn = null;
     ResultSet rs = null;
     PreparedStatement pst = null;
-    
-    
+
     @FXML
     private TextField txtFieldUsuario;
-    
+
+    @FXML
+    private PasswordField txtFieldPassword;
+
     @FXML
     private Text txtMensajeLogin;
-    
 
-        
     /*
     Al ejecutar la aplicación se creará la conexión con la base de datos
-    */
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         conn = ConexionMySQL.ConnecrDb();
-    }    
-    
+    }
+
     /*
     Cuando el usuario pulse el botón se consultará en base de datos
-    */
+     */
     @FXML
-    public void mostrarUsuario(ActionEvent event) throws SQLException {
+    public void mostrarUsuario(ActionEvent event) throws SQLException, IOException {
         //Consulta SQL para seleccionar el valor de la columna usuario en la tabla credenciales, donde
         // el valor de la columna id_credencial sea 1
-        String sql = "SELECT usuario FROM credenciales where id_credencial=1";
+        String sql = "SELECT usuario FROM credenciales where usuario='" + txtFieldUsuario.getText() + "' and password='"
+                + txtFieldPassword.getText() + "'";
         pst = conn.prepareStatement(sql);
         rs = pst.executeQuery(sql); //En rs estará el valor de la tabla
 
         if (rs.next()) {                            //Si ha encontrado un valor
             String usuBd = rs.getString("usuario");   //En lbl estará el valor de la columna usuario
             String mensaje = "";
-            if(usuBd != null && txtFieldUsuario!=null && usuBd.equals(txtFieldUsuario.getText())){
-                mensaje = "Usuario encontrado";   
-                
+            if (usuBd != null && txtFieldUsuario != null && usuBd.equals(txtFieldUsuario.getText())) {
+                mensaje = "Usuario encontrado";
+                //CREAR LA VENTANA DE ALTA
                 Parent root;
-                try {
-                    FXMLLoader.load(getClass().getResource("/appconbd/FXMLDocument.fxml"));
-                    root = FXMLLoader.load(getClass().getResource("/appconbd/FXMLAlta.fxml"));
-                    Stage stage = new Stage();
-                    stage.setTitle("Alta");
-                    stage.setScene(new Scene(root, 450, 450));
-                    stage.show();
-                    // Hide this current window (if this is what you want)
-                    ((Node)(event.getSource())).getScene().getWindow().hide();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }else{
-                mensaje = "Usuario no encontrado";   
+                root = FXMLLoader.load(getClass().getResource("/appconbd/FXMLAlta.fxml"));
+                FXMLLoader loader2 = new FXMLLoader(getClass().getResource("/appconbd/FXMLAlta.fxml"));
+                Parent root2 = loader2.load();
+                Scene miEscena = new Scene(root2);
+                //AnchorPane root3 = (AnchorPane)loader2.load();
+                //Text foo2;
+                //foo2 = (Text)loader2.getNamespace().get("txtBienvenida");
+                //foo2.setText(foo2.getText()+txtFieldUsuario.getText());
+
+                Stage stage = new Stage();
+                stage.setTitle("Alta");
+                stage.setScene(new Scene(root, 450, 450));
+                stage.show();
+                // Hide this current window (if this is what you want)
+                ((Node) (event.getSource())).getScene().getWindow().hide();
+
             }
-            
-            txtMensajeLogin.setText(mensaje);                     //Establecer el texto de la etiqueta que se muestra por pantalla
-        }else{
-            txtMensajeLogin.setText("Usuario no encontrado en base de datos"); 
         }
+        conn.close();
     }
+
 }
